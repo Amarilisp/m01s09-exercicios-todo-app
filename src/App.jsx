e9039fb4fd5e575fd8ab9e8a33b3fc08795d2ddd
Amarilis Pequeno <amarilispequeno@gmail.com>
2023-03-31 20:12:44 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import { Outlet } from "react-router-dom";
 
 export const App = () => {
   return (
     <div>
       <Header />
       <main className="container mt-4">
+        <Outlet />
         {/* algo precisa ser modificado aqui... */}
       </main>
     </div>
